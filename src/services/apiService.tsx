650ae3897562a22115f1730564c4944f51b2b1b5
Keryu7 <yyurikt@tut.by>
2024-07-12 00:42:52 +0300

    feat: add pagination

--- src/services/apiService.tsx
+++ src/services/apiService.tsx
@@ -1,9 +1,13 @@
 import { Result } from '../types';
 
-export const getStarships = async (query: string): Promise<Result[]> => {
-  const fetchResults: Response = await fetch(`https://swapi.dev/api/starships?search=${query}`);
+export const getStarships = async (query: string, page: number): Promise<{ results: Result[], total_pages: number }> => {
+  console.log('VALUE', page)
+  const fetchResults = await fetch(`https://swapi.dev/api/starships/?search=${query}&page=${page}`);
   const response = await fetchResults.json();
-  return response.results;
+  return {
+    results: response.results,
+    total_pages: Math.ceil(response.count / 10),
+  };
 };
 
 export const getStarshipDetails = async (id: string | undefined): Promise<Result> => {
